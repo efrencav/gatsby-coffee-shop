7eaf57a31a75d8be2f20119309ce73019cd2ec15
efrencav <efren@abstract-creative.com>
2019-11-29 20:46:00 -0600

    update

--- src/components/Globals/Footer.js
+++ src/components/Globals/Footer.js
@@ -1,16 +1,17 @@
-import React from 'react'
+import React from "react";
 
 export default function Footer() {
-    return (
-        <footer className="footer py-3">
-            <div className="div container">
-                <div className="div row">
-                    <div className="col-10 mx-auto col-md-6 text-yellow text-center text-capitalize">
-                        <h3>all rights reserved &copy;{new Date().getFullYear().toString}</h3>
-                    </div>
-                </div>
-            </div>
-        </footer>
-
-    )
+  return (
+    <footer className="footer py-3">
+      <div className="container">
+        <div className="row">
+          <div className="col-10 mx-auto col-md-6 text-yellow text-center text-capitalize">
+            <h3>
+              all rights reserved &copy;{new Date().getFullYear().toString()}
+            </h3>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
--- src/components/layout.js
+++ src/components/layout.js
@@ -5,7 +5,7 @@ import { StaticQuery, graphql } from "gatsby";
 import "./bootstrap.min.css";
 import "./layout.css";
 
-import Navbar from './Globals/navbar';
+import Navbar from './Globals/Navbar';
 import Footer from './Globals/Footer';
 
 
