2ba0274d06cda29b4b9e7a211a9d75462b9175d0
efrencav <efren@abstract-creative.com>
2019-11-29 21:28:45 -0600

    updated styles

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,17 +5,16 @@ import Layout from "../components/layout";
 import SEO from "../components/seo";
 import BackgroundSection from "../components/Globals/BackgroundSection";
 import Info from '../components/Home/Info';
-
 const AboutPage = ({ data }) => (
-    <Layout>
-        <SEO title="About" />
-        <BackgroundSection
-            image={data.image.childImageSharp.fluid}
-            title="about us"
-            styleClass="about -background"
-        />
-        <Info />
-    </Layout>
+  <Layout>
+    <SEO title="About" />
+    <BackgroundSection
+      image={data.image.childImageSharp.fluid}
+      title="about us"
+      styleClass="about-background"
+    />
+    <Info />
+  </Layout>
 );
 
 export const query = graphql`
